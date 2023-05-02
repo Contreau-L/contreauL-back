800da2bd60c2729b853d7ae7c2d335c2cecfab4a
Marius-courquin <marius.courquin@ig2i.centralelille.fr>
2023-05-02 22:20:26 +0200

    fix: patch requests

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     next();
 });
 
